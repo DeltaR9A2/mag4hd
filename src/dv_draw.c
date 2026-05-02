6187bb5acd2fe59846981728d9b315e7d0a601ec
DeltaR9A2 <they.called.him.crazy@gmail.com>
2026-05-02 17:24:13 -0500

    Initial commit. Mostly copied from valdoria.

---
+++ src/dv_draw.c
@@ -0,0 +1,3 @@
+#include "dv_fb.h"
+
+
---
+++ src/main.c
@@ -0,0 +1,53 @@
+
+#include "dv_glfw.h"
+#include "dv_font.h"
+
+const char test_string[] = "If you want your cache to store only one copy per unique string, you must compare the string contents, not the pointers. Thankfully, stb_ds.h does exactly that - if the key is a char *, it automatically uses strcmp() for comparisons and stbds_hash_string() for hashing.";
+
+void input_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+  (void)mods;
+  (void)window;
+  (void)scancode;
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+    dv_glfw_stop_game();
+  }
+}
+
+int main(void)
+{
+  dv_fb_t *screen = dv_glfw_init();
+
+  dv_glfw_add_keyfun(&input_callback);
+
+  //  dv_fb_t *cursor = dv_get_image("cursor-arrow.png");
+
+  font_t *title_font = font_create("font-title.png", RGBA(0xFF,0xBF,0x00,0xFF), RGBA(0x3B,0x1F,0x00,0xFF));
+  font_t *prose_font = font_create("font-prose.png", RGBA(0xDD,0xDD,0xDD,0xFF), RGBA(0x22,0x22,0x22,0xFF));
+  font_t *small_font = font_create("font-small.png", RGBA(0xFF,0xFF,0xFF,0xFF), RGBA(0x22,0x22,0x22,0xFF));
+
+  dv_fb_fill(screen, RGBA(0x33,0x66,0x33,0xFF));
+
+  font_draw_string(title_font, "Welcome to This Game", 20, 40, screen);
+  font_draw_string(prose_font, test_string, 20, 60, screen);
+  font_wrap_string(prose_font, test_string, 20, 80, 250, screen);
+  font_wrap_string(small_font, test_string, 290, 80, 250, screen);
+
+  dv_fb_set_pixel(screen, 5, 15, RGBA(0xFF,0xFF,0xFF,0xFF));
+
+  //  dv_fb_blit_blend(screen, 20, 20, cursor);
+
+  int32_t cms = 0, pms = 0, msd = 0, msa = 0, mspf = 10;
+  while(RUNNING){
+    pms = cms; cms = SDL_GetTicks(); msd = cms - pms; msa += msd;
+    if(msa > mspf){ msa -= mspf;
+
+
+  while (dv_glfw_keep_going())
+  {
+    dv_glfw_draw_window();
+  }
+
+  dv_glfw_exit();
+  return 0;
+}
+
